4a154ffc6b3600a1ece10036d1b4ea7b5a7f8a19
95hyouka <95hyouka@gmail.com>
2026-01-30 19:14:45 +0900

    set up some default apps

--- flake-outputs/nix-darwin/default.nix
+++ flake-outputs/nix-darwin/default.nix
@@ -40,6 +40,46 @@ in {
         };
       }
       (import ./modules/aerospace)
+      {
+        environment.systemPackages = [
+          pkgs.duti
+        ];
+
+        # https://github.com/nix-darwin/nix-darwin/issues/663
+        # https://github.com/nix-darwin/nix-darwin/blob/0fc4e7ac670a0ed874abacf73c4b072a6a58064b/modules/system/activation-scripts.nix#L118
+        system.activationScripts = {
+          # https://github.com/nix-darwin/nix-darwin/issues/1506
+          # Setting default apps using nix-darwin is currently not implemented.
+          # So this:
+          postActivation.text = ''
+            set -euo pipefail
+
+            function set_default_app() {
+              local target_bundle_id="$1"
+              local uti="$2"
+              local target_user="$3"
+
+              current_default_app=$(sudo -u "$target_user" ${pkgs.duti}/bin/duti -d "$uti" || true)
+              if [ "$current_default_app" != "$target_bundle_id" ]; then
+                sudo -u "$target_user" ${pkgs.duti}/bin/duti -s "$target_bundle_id" "$uti" all
+              fi
+            }
+
+            tuples=(
+              "stirling.pdf.dev:com.adobe.pdf"
+              "com.microsoft.VSCode:public.plain-text"
+              "com.microsoft.VSCode:public.source-code"
+              "com.microsoft.VSCode:public.data"
+            )
+
+            for t in "''${tuples[@]}"; do
+              bundle_id="''${t%%:*}"
+              uti="''${t#*:}"
+              set_default_app "$bundle_id" "$uti" "${profile.userName}"
+            done
+          '';
+        };
+      }
     ];
   };
 }
